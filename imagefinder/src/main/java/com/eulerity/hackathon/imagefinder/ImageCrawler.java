5a0fdb9ce9edcf7c3b8d643b4232b8623994a516
Joseph Francis Re <josephfrancisre@gmail.com>
2023-02-14 19:54:31 -0500

    Main setup

---
+++ imagefinder/src/main/java/com/eulerity/hackathon/imagefinder/ImageCrawler.java
@@ -0,0 +1,53 @@
+package com.eulerity.hackathon.imagefinder;
+
+import java.util.ArrayList;
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.Connection;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class ImageCrawler implements Runnable {
+	private int ID;
+	private Thread thread;
+	private Document source;
+	private String title;
+
+	public ImageCrawler(Document doc, int id) {
+		source = doc;
+		title = source.title();
+		ID = id;
+		System.out.println("ImageCrawler with ID of " + ID + " and source of " + title + " created.");
+		deployThread();
+	}
+	
+	private void deployThread() {
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	private void crawl(Document doc) {
+		if (doc != null) {
+			Elements images = doc.getElementsByTag("img");
+			
+			for (Element src: images) {
+				if (crawled_images.contains(src) == false) {
+					System.out.println("Image src = " + src.attr("abs:src"));
+					crawled_images.add(src.attr("abs:src"));
+				}
+			}
+		}
+	}
+	
+	public Thread getThread() {
+		return thread;
+	}
+
+	@Override
+	public void run() {
+		crawl(source);
+	}
+	
+}
