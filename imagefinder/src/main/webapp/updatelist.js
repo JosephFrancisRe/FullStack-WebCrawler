f34f08419ce0ec46ecc3a8509e00231a9497bd2b
Joseph Francis Re <josephfrancisre@gmail.com>
2023-02-20 00:03:42 -0500

    Web interface finalized sans anchor adjustment

---
+++ imagefinder/src/main/webapp/updatelist.js
@@ -0,0 +1,63 @@
+var imageList = document.querySelector('ul.images');
+var logoList = document.querySelector('ul.logos');
+var urlInput = document.querySelector('input[name=url]');
+var imagesDoc = document.getElementById("images");
+var logosDoc = document.getElementById("logos");
+
+apiCallBack = function(xhr, callback) {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+        if (xhr.status != 200) {
+            let message = xhr.status + ":" + xhr.statusText + ":" + xhr.responseText;
+            alert(message);
+            throw 'API call returned bad code: ' + xhr.status;
+        }
+        let response = xhr.responseText ? JSON.parse(xhr.responseText) : null;
+        if (callback) {
+            callback(response);
+        }
+    }
+}
+
+updateList = function(response) { 
+    imageList.innerHTML = '';
+    var logoIdentifiers = ['.svg', 'logo', 'favicon'];
+    for (var i = 0; i < response.length; i++) {
+        if (response[i]) {
+            var img = document.createElement("img");
+            img.width = 200;
+            img.src = response[i];
+            if (!logoIdentifiers.some(v => response.includes(v))) {
+                imageList.appendChild(img);
+            } else {
+                logoList.appendChild(img);
+            }
+        }
+    }
+}
+
+makeApiCall = function(url, method, obj, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onreadystatechange = apiCallBack.bind(null, xhr, callback);
+    xhr.send(obj ? obj instanceof FormData || obj.constructor == String ? obj : JSON.stringify(obj) : null);
+}
+
+document.querySelector('.lucky').addEventListener("click", function(event) {
+    event.preventDefault();
+    var verifiedSites = ['http://books.toscrape.com/', 'https://ashesofcreation.com/'];
+    var randomIndex = Math.floor(Math.random() * verifiedSites.length);
+    document.getElementById('url').setAttribute('value', verifiedSites[randomIndex]);
+    makeApiCall('/main?url=' + verifiedSites[randomIndex], 'POST', null, updateList);
+});
+
+document.querySelector('.submit').addEventListener("click", function(event) {
+    event.preventDefault();
+    makeApiCall('/main?url=' + urlInput.value, 'POST', null, updateList);
+});
+
+document.querySelector('.reset').addEventListener("click", function(event) {
+    event.preventDefault();
+    document.querySelector('ul.images').innerHTML = '';
+    document.querySelector('ul.logos').innerHTML = '';
+    makeApiCall('/main?url=', 'DELETE', null, null);
+});
